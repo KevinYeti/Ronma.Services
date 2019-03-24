78d3acfcb594dfde0af9a40c9bd49eeb042b4432
KevinShen <kevinyeti@gmail.com>
2019-03-25 01:24:57 +0800

    wip

--- src/Sensor/HotwordDetection/main.py
+++ src/Sensor/HotwordDetection/main.py
@@ -1,50 +1,11 @@
-#!/usr/bin/env python
-# encoding: utf-8
-# @Time : 19-3-24 上午12:27
-
-__author__ = 'KevinShen'
-
 from res import snowboydecoder
-import sys
+import os, sys
 import signal
-import speech_recognition as sr
-import os
 
-"""
-This demo file shows you how to use the new_message_callback to interact with
-the recorded audio after a keyword is spoken. It uses the speech recognition
-library in order to convert the recorded audio into text.
-
-Information on installing the speech recognition library can be found at:
-https://pypi.python.org/pypi/SpeechRecognition/
-"""
-
+# Demo code for listening to two hotwords at the same time
 
 interrupted = False
 
-
-def audioRecorderCallback(fname):
-    print("converting audio to text")
-    r = sr.Recognizer()
-    with sr.AudioFile(fname) as source:
-        audio = r.record(source)  # read the entire audio file
-    # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print(r.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-    os.remove(fname)
-
-
-
-def detectedCallback():
-  print('recording audio...', end='', flush=True)
 
 def signal_handler(signal, frame):
     global interrupted
@@ -55,26 +16,32 @@ def interrupt_callback():
     global interrupted
     return interrupted
 
-if len(sys.argv) == 1:
-    print("Error: need to specify model name")
-    print("Usage: python demo.py your.model")
+if len(sys.argv) < 2:
+    print("Error: need to specify model names and sensitivity")
+    print("Usage: python main.py 0.5")
     sys.exit(-1)
 
-model = sys.argv[1]
+sensitivity = sys.argv[1]
+files = os.listdir("./res/*.*mdl")
+models = sys.argv[2:]
+
+
 
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.38)
+sensitivity = sensitivity*len(models)
+detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
+callbacks = []
+for i in range(len(models)):
+    callbacks.append(lambda: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING))
+
 print('Listening... Press Ctrl+C to exit')
 
 # main loop
-detector.start(detected_callback=detectedCallback,
-               audio_recorder_callback=audioRecorderCallback,
+# make sure you have the same numbers of callbacks and models
+detector.start(detected_callback=callbacks,
                interrupt_check=interrupt_callback,
-               sleep_time=0.01)
+               sleep_time=0.03)
 
 detector.terminate()
-
-
-
